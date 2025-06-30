4df291712b442d7dcdb9ba999d7c9517c65bb9fa
abhijai-k <abhijai2001@gmail.com>
2025-06-30 23:18:33 +0530

    Mini message board

---
+++ full-stack-javascript/nodejs/mini-message-board/routes/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const router = express.Router();
+
+const messages = [
+  { text: 'Hi there!', user: 'Amando', added: new Date() },
+  { text: 'Hello World!', user: 'Charles', added: new Date() }
+];
+
+router.get('/', (req, res) => {
+  res.render('index', { title: 'Mini Messageboard', messages });
+});
+
+router.get('/new', (req, res) => {
+  res.render('form', { title: 'New Message' });
+});
+
+router.post('/new', (req, res) => {
+  const { messageUser, messageText } = req.body;
+  messages.push({ text: messageText, user: messageUser, added: new Date() });
+  res.redirect('/');
+});
+
+router.get('/message/:index', (req, res) => {
+  const message = messages[req.params.index];
+  if (!message) return res.status(404).send('Message not found');
+  res.render('message', { title: 'Message Detail', message });
+});
+
+module.exports = router;
